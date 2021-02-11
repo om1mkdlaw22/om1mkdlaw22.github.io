0407af47f461941e549017221b7d98050eb3f7fb
om1mkdlaw22 <78913552+om1mkdlaw22@users.noreply.github.com>
2021-02-11 20:52:25 +0700

    Update config.php

--- config.php
+++ config.php
@@ -20,8 +20,8 @@ $use_auth = true;
 // Users: array('Username' => 'Password', 'Username2' => 'Password2', ...)
 // Generate secure password hash - https://tinyfilemanager.github.io/docs/pwd.html
 $auth_users = array(
-    'admin' => '$2y$10$/K.hjNr84lLNDt8fTXjoI.DBp6PpeyoJ.mGwrrLuCZfAwfSAGqhOW', //admin@123
-    'user' => '$2y$10$Fg6Dz8oH9fPoZ2jJan5tZuv6Z4Kp7avtQ9bDfrdRntXtPeiMAZyGO' //12345
+    'admin' => '$2y$10$SQn.sv/68BZYVbZ7dIoo4OR3xYx1d0kiQXvNJF5njf327WbLaF7Le', //admin@123
+    'user' => '$2y$10$abk4x0d7sQhBSJdSkC4hXe8PXrIY./IL1R9CXBMVKuQjypzIckiBO' //12345
 );
 
 //set application theme
